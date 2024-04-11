76a231935e9ab716f4420c26d5ca887279d927bb
Boryana Kircheva (HAU-ED) <boryana.kircheva@liebherr.com>
2024-04-11 22:54:09 +0300

    main change

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+window.__VUE_PROD_DEVTOOLS__ = false;
+window.__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = false;
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
